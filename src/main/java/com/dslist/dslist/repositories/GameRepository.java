3038b0ea2dfc19212367af38eeaf04c8e3b9162d
Gabriel Rabelo <gabrielrabeloti@gmail.com>
2025-01-20 18:50:39 -0300

    Game repository, service, controller

---
+++ src/main/java/com/dslist/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.dslist.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dslist.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
